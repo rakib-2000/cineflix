234098d5b61186c52451e222c41fe010053bbcc6
rakx07 <rakib15-13846@diu.edu.bd>
2024-07-12 20:26:43 +0600

    Initializing files.

---
+++ src/test/java/com/rakx07/cineflix/CineflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakx07.cineflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
